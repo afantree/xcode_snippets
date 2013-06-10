b4403658772a73f223188c5d3aadca9ee16b4a75
sunchaofan <729397005@qq.com>
2013-06-10 18:53:16 +0800

    update

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 SRC_HOME=`pwd`
-sudo cp ~/Library/Developer/Xcode/UserData/CodeSnippets ${SRC_HOME}/CodeSnippets
+cp -a ~/Library/Developer/Xcode/UserData/CodeSnippets ${SRC_HOME}/CodeSnippets
 
 git commit -a -m "update"
 git push -u origin master
