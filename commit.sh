18891bcc71e543ddcf355f2125de76fd4b5d327a
阿凡树 <729397005@qq.com>
2015-03-15 18:10:46 +0800

    update

--- commit.sh
+++ commit.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 SRC_HOME=`pwd`
-cp -a ~/Library/Developer/Xcode/UserData/CodeSnippets ${SRC_HOME}/CodeSnippets
+cp -a ~/Library/Developer/Xcode/UserData/CodeSnippets ${SRC_HOME}/
 
 git commit -a -m "update"
 git push -u origin master
